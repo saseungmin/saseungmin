c786ba707e7e7e1203fc577e39338ef1981d0607
saseungmin <dbd02169@naver.com>
2021-01-27 23:47:53 +0900

    [Feature] My Blog initial commit - Used to gatsby-starter-bee

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `SeungMin Sa`,
+  description: `Blog posted about ...`,
+  author: `saseungmin`,
+  introduction: `Hello! It's Me! and I want to be a Front-End developer! `,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `saseungmin`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: `profile.php?id=100003758628929`, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/profile.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'saseungmin',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
